5c41e44b84116540ce36e3790ae70645d32a163f
kwtjdtn <kwtjdtn1@naver.com>
2020-02-20 21:44:36 +0900

    frame of YouHa

--- src/component/upload.js
+++ src/component/upload.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
 import "./upload.css";
 
-const Upload=()=> {
-    return (
-        <div className="filebox">
-            <div className = "upload-title">동영상 업로드 하기</div>
-            <label for="ex_file">업로드</label>
-            <input type="file" id="ex_file"/>
-        </div>
-    );
+const Upload = () => {
+  const onDrop = useCallback(acceptedFiles => {
+    // Do something with the files
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    // <div className ="box-wrapper">
+      <div {...getRootProps()} className="upload-wrapper">
+        <input {...getInputProps()} className="upload-box" />
+        {isDragActive ? (
+          <p>파일 여기다 두셈</p>
+        ) : (
+          <div className="upload-box">
+            파일 여기로 끌고 오거나 클릭해서 파일 선택하셈
+          </div>
+        )}
+      </div>
+    // </div>
+  );
+  // return (
+  //     <div className="filebox">
+  //         <div className = "upload-title">동영상 업로드 하기</div>
+  //         <label for="ex_file">업로드</label>
+  //         <input type="file" id="ex_file"/>
+  //     </div>
+  // );
 };
 
 export default Upload;
