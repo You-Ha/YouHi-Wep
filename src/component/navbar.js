9e676a49b2fca86d4ba2f39ac32e17e0bbbf225b
kwtjdtn <kwtjdtn1@naver.com>
2020-02-20 23:08:35 +0900

    frame update

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -7,7 +7,6 @@ const Navbar=() => {
         <div className = "nav-wrapper">
             <img src = {Logo} className = "nav-logo" alt = 'Logo'/> 
             <input type="text" className = "nav-search" placeholder = "YouHa" />
-            <div className = "nav-link">내 동영상</div>
         </div>
     );
 };
