5293e9f7478a83939668d05d1ffbca2a2f613424
leeinpyeong <jinipyung@gmail.com>
2020-03-07 21:33:38 +0900

    Add scrollbars

--- src/component/video.js
+++ src/component/video.js
@@ -1,11 +1,46 @@
 import React from "react";
+import { Scrollbars } from "react-custom-scrollbars";
 import "./video.css";
 
 const Video = () => {
   return (
     <div>
       <div id="videoFilter_title">영상 필터</div>
-      <div className="video-filter"></div>
+      <div className="video-filter">
+        <Scrollbars
+          style={{ height: 450 }}
+          renderTrackHorizontal={props => (
+            <div
+              {...props}
+              style={{ display: "none" }}
+              className="track-horizontal"
+            />
+          )}
+        >
+          <p>0</p>
+          <p>1</p>
+          <p>2</p>
+          <p>3</p>
+          <p>4</p>
+          <p>5</p>
+          <p>6</p>
+          <p>7</p>
+          <p>8</p>
+          <p>9</p>
+          <p>9</p>
+          <p>9</p>
+          <p>9</p>
+          <p>9</p>
+          <p>9</p>
+          <p>9</p>
+          <p>9</p>
+          <p>9</p>
+          <p>9</p>
+          <p>9</p>
+          <p>9</p>
+          <p>9</p>
+        </Scrollbars>
+      </div>
     </div>
   );
 };
--- src/component/voice.js
+++ src/component/voice.js
@@ -1,11 +1,46 @@
 import React from "react";
+import { Scrollbars } from "react-custom-scrollbars";
 import "./voice.css";
 
 const Voice = () => {
   return (
     <div>
-      <div id="voiceFilter_title">음성</div>
-      <div className="voice-filter"></div>
+      <div id="voiceFilter_title">음성 필터</div>
+      <div className="voice-filter">
+        <Scrollbars
+          style={{ height: 356 }}
+          renderTrackHorizontal={props => (
+            <div
+              {...props}
+              style={{ display: "none" }}
+              className="track-horizontal"
+            />
+          )}
+        >
+          <p>0</p>
+          <p>1</p>
+          <p>2</p>
+          <p>3</p>
+          <p>4</p>
+          <p>5</p>
+          <p>6</p>
+          <p>7</p>
+          <p>8</p>
+          <p>9</p>
+          <p>9</p>
+          <p>9</p>
+          <p>9</p>
+          <p>9</p>
+          <p>9</p>
+          <p>9</p>
+          <p>9</p>
+          <p>9</p>
+          <p>9</p>
+          <p>9</p>
+          <p>9</p>
+          <p>9</p>
+        </Scrollbars>
+      </div>
     </div>
   );
 };
