43dc501d42266cdb4c1c311473dc000d898072e3
leeinpyeong <jinipyung@gmail.com>
2020-03-23 05:51:50 +0900

    checkbox customization 구현

--- src/component/upload/text/Text.js
+++ src/component/upload/text/Text.js
@@ -4,9 +4,9 @@ import "./Text.css";
 
 const Text = () => {
   return (
-    <div className="text-wrapper">
-      <div className="text-title">도움말 및 제안사항</div>
-      <div className="text-box">
+    <div className="Text-wrapper">
+      <div className="Text-title">도움말 및 제안사항</div>
+      <div className="Text-box">
         <ul>
           <li>
             필터링에 소요되는 시간은 약 10분이며, 사용자의 인터넷 환경에 따라
@@ -14,8 +14,16 @@ const Text = () => {
           </li>
           <li>업로드 파일의 형식은 .avi와 .mp4로 제한합니다.</li>
           <li>
-            <div className="emphasis">연령제한 옵션</div>을 선택하면 필터링없이 연령제한이 적용된 컨텐츠로
-            업로드됩니다.
+            <div className="Text-text-emphasis">연령제한 옵션</div>을 선택하면
+            필터링없이 연령제한이 적용된 컨텐츠로 업로드됩니다.
+            <br />
+            <label className="Text-checkbox-container">
+              <div className="Text-text-emphasis">
+                연령제한 컨텐츠로 설정합니다.
+              </div>
+              <input type="checkbox" className="Text-checkbox" />
+              <div className="Text-checkmark"></div>
+            </label>
           </li>
         </ul>
       </div>
