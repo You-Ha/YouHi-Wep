ab1306c2ce263272b23f655be95123673398267d
leeinpyeong <jinipyung@gmail.com>
2020-03-23 03:15:43 +0900

    Navbar 컴포넌트 navbar 폴더로 이동

---
+++ src/component/navbar/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Navbar.css";
+//import Logo from '../img/logo.png';
+
+const Navbar = () => {
+  return (
+    <div className="nav-wrapper">
+      {/* <div className="nav-wrapper">
+        <img src={Logo} className="nav-logo" alt="Logo" />
+        <input type="text" className="nav-search" placeholder="YouHa" />
+      </div> */}
+      <img id="main-logo" src={require("../../img/YouHa_logo.png")} alt="" />
+      <div id="project-name">YouHi</div>
+    </div>
+  );
+};
+
+export default Navbar;
