0d8b9ac28f1162aea1f3f7dd0b483ac9e6654891
leeinpyeong <jinipyung@gmail.com>
2020-03-23 06:31:47 +0900

    filter box 추가

--- src/component/filter/Filter.js
+++ src/component/filter/Filter.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import Video from './video/Video';
-import Voice from './voice/Voice';
-import FilterElement from './filter_elementor/FilterElementor'
+import React from "react";
+import Video from "./video/Video";
+import Voice from "./voice/Voice";
+import FilterElement from "./filter_elementor/FilterElementor";
 
 const Filter = () => {
   return (
     <div className="Filter-wrapper">
       <FilterElement />
+      <img
+        alt="video-filter-box"
+        src={require("../../img/Video_filter_box.png")}
+      />
+      <img
+        alt="voice-filter-box"
+        src={require("../../img/Voice_filter_box.png")}
+      />
       {/* <Video />
       <Voice /> */}
     </div>
