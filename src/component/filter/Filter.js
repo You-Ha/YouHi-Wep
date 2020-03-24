09e88ac388716d831861c10b0f999ea906e56687
leeinpyeong <jinipyung@gmail.com>
2020-03-25 03:14:42 +0900

    class component로 수정

--- src/component/filter/Filter.js
+++ src/component/filter/Filter.js
@@ -1,30 +1,39 @@
-import React from "react";
-import Video from "./video/Video";
-import Voice from "./voice/Voice";
+import React, { Component } from "react";
 import FilterElement from "./filter_elementor/FilterElementor";
 import "./Filter.css";
 
-const Filter = () => {
-  return (
-    <div className="Filter-container">
-      <FilterElement />
-      <div className="Filter-filter-box">
-        <img
+class Filter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+    // this.showFilterResult = this.showFilterResult.bind(this);
+  }
+
+  render() {
+    return (
+      <div className="Filter-container">
+        <FilterElement />
+        <div className="Filter-filter-box">
+          {/* <img
           alt="video-filter-box"
           src={require("../../img/Video_filter_box.png")}
         />
         <img
           alt="voice-filter-box"
           src={require("../../img/Voice_filter_box.png")}
-        />
+        /> */}
+          <button className="Filter-box Filter-video-filter" />
+          <button className="Filter-box Filter-voice-filter" />
+        </div>
+        <div className="Filter-feedback-button-wrapper">
+          <button className="Filter-feedback-button">
+            이의신청 및 문의하기
+          </button>
+        </div>
       </div>
-      <div className="Filter-feedback-button-wrapper">
-        <button className="Filter-feedback-button">이의신청 및 문의하기</button>
-      </div>
-      {/* <Video />
-      <Voice /> */}
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Filter;
