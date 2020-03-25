aa17c3a6c7b3cf6381ebfd1bae0e00af4d8c8ae5
leeinpyeong <jinipyung@gmail.com>
2020-03-26 01:01:54 +0900

    필터링 결과 팝업창 사진 업로드

--- src/component/filter/Filter.js
+++ src/component/filter/Filter.js
@@ -1,29 +1,34 @@
 import React, { Component } from "react";
 import FilterElement from "./filter_elementor/FilterElementor";
 import "./Filter.css";
+import NewWindow from "react-new-window";
 
 class Filter extends Component {
   constructor(props) {
     super(props);
     this.state = {};
 
-    // this.showFilterResult = this.showFilterResult.bind(this);
+    this.showFilterResult = this.showFilterResult.bind(this);
   }
+
+  showFilterResult() {
+    var wFeature =
+    'width=500, height=500, location=no,toolbar=no,menubar=no,scrollbars=yes,resizable=yes';
+    var w = window.open("/filter", "_blank", wFeature);
+    w.document.write(`<title>Filter</title>`);
+    w.document.write(`<img src="static/media/background_img.3ccc4efd.png" width="300px"> 안녕`)
+
+  } 
 
   render() {
     return (
       <div className="Filter-container">
         <FilterElement />
         <div className="Filter-filter-box">
-          {/* <img
-          alt="video-filter-box"
-          src={require("../../img/Video_filter_box.png")}
-        />
-        <img
-          alt="voice-filter-box"
-          src={require("../../img/Voice_filter_box.png")}
-        /> */}
-          <button className="Filter-box Filter-video-filter" />
+          <button
+            className="Filter-box Filter-video-filter"
+            onClick={this.showFilterResult}
+          />
           <button className="Filter-box Filter-voice-filter" />
         </div>
         <div className="Filter-feedback-button-wrapper">
