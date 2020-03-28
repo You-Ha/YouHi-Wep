92606f5636d03f897b01733d3599d895e325d503
leeinpyeong <jinipyung@gmail.com>
2020-03-28 15:23:52 +0900

    a tag remove underline

--- src/component/introduction/Introduction.js
+++ src/component/introduction/Introduction.js
@@ -24,7 +24,7 @@ class test extends Component {
                     <div className="elementor-heading-bar"></div>
                   </div>
                   <div className="elementor-text-editor">
-                    유하이는 영상분류 신경망 모델을 이용한 정확한 필터링을
+                    YouHi는 영상분류 신경망 모델을 이용한 정확한 필터링을
                     제공함으로써
                     <br /> 업로더의 동영상에서 청소년에게 부적합한 내용을 모두
                     걸러냅니다.
@@ -34,6 +34,7 @@ class test extends Component {
                     <br />
                     연령 제한 적용 고려 사항이며 자세한 내용은{" "}
                     <a
+                      className="Introduction-link"
                       href="https://support.google.com/youtube/answer/2802167?hl=ko"
                       rel="noopener noreferrer "
                       target="_blank"
