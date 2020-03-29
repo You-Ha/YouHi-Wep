ca261af68780cb41f8e0a52776ec8deaaf20d850
leeinpyeong <jinipyung@gmail.com>
2020-03-30 00:39:47 +0900

    react-modal 적용

--- src/component/filter/Filter.js
+++ src/component/filter/Filter.js
@@ -1,40 +1,80 @@
 import React, { Component } from "react";
 import FilterElement from "./filter_elementor/FilterElementor";
-import Modal from "react-modal"
+import Modal from "react-modal";
+import { Scrollbars } from "react-custom-scrollbars";
 import "./Filter.css";
 // import NewWindow from "react-new-window";
+
+Modal.setAppElement("#root");
 
 class Filter extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      modalIsOpen: false
+    };
 
     this.showFilterResult = this.showFilterResult.bind(this);
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({ modalIsOpen: true });
+  }
+
+  afterOpenModal() {}
+
+  closeModal() {
+    this.setState({ modalIsOpen: false });
   }
 
   showFilterResult() {
     var wFeature =
-    'width=500, height=500, location=no,toolbar=no,menubar=no,scrollbars=yes,resizable=yes';
+      "width=500, height=500, location=no,toolbar=no,menubar=no,scrollbars=yes,resizable=yes";
     var w = window.open("/filter", "_blank", wFeature);
     w.document.write(`<title>Filter</title>`);
-    w.document.write(`<img src="static/media/background_img.3ccc4efd.png" width="300px"> 안녕`)
-
-  } 
+    w.document.write(
+      `<img src="static/media/background_img.3ccc4efd.png" width="300px"> 안녕`
+    );
+  }
 
   render() {
     return (
       <div className="Filter-container">
         <FilterElement />
-        <div className = "filter-wrapper">
+        <div className="filter-wrapper">
           <div className="Filter-filter-box">
-            <div className = "inner-filter-wrapper">
+            <div className="inner-filter-wrapper">
               <button
                 className="Filter-box Filter-video-filter"
-                onClick={this.showFilterResult}
+                onClick={this.openModal}
               />
+              <Modal
+                isOpen={this.state.modalIsOpen}
+                onAfterOpen={this.afterOpenModal}
+                onRequestClose={this.closeModal}
+                className="Filter-modal"
+                // overlayClassName="'Filter-modal-overlay"
+              >
+                <Scrollbars
+                  style={{ height: 420 }}
+                  renderTrackHorizontal={props => (
+                    <div
+                      {...props}
+                      style={{ display: "none" }}
+                      className="track-horizontal"
+                    />
+                  )}
+                >
+                  hello<br />
+                </Scrollbars>
+                {/* <button onClick={this.closeModal}>close</button> */}
+              </Modal>
             </div>
-            
-            <div className = "inner-filter-wrapper">
+
+            <div className="inner-filter-wrapper">
               <button className="Filter-box Filter-voice-filter" />
             </div>
           </div>
